1f98c5d27eb619104345167479137b15c04968d9
Girdhar Shukla <girdharshukla26@gmail.com>
2026-05-17 01:41:29 +0530

    arena bug fix

--- db/arena.h
+++ db/arena.h
@@ -11,7 +11,7 @@ struct arena_type{
 };
 
 void arena_init(struct arena_type *arena, void *buffer, size_t buffer_length);
-void *arena_alloc_aligned(struct arena_type *arena, size_t alignment, size_t size);
+void *arena_alloc_aligned(struct arena_type *arena, size_t size, size_t alignment);
 void *arena_alloc(struct arena_type *arena, size_t size);
 void arena_reset(struct arena_type *arena);
 
